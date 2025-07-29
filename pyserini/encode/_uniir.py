be69b05af48d5be6a125b5fb55dd5c7ad6fa5b8a
clides <daniel168.guo@gmail.com>
2025-07-29 13:18:07 -0400

    refractor code

--- pyserini/encode/_uniir.py
+++ pyserini/encode/_uniir.py
@@ -9,30 +9,30 @@ from torch.utils.data import DataLoader, Dataset
 
 from pyserini.uniir import (BLIPFeatureFusion, BLIPScoreFusion,
                             CLIPFeatureFusion, CLIPScoreFusion,
-                            MBEIRCandidatePoolCollator,
-                            format_string,
+                            MBEIRCandidatePoolCollator, format_string,
                             hash_did)
 
-class CustomCorpusDataset(Dataset):  
+
+class CustomCorpusDataset(Dataset):
     def __init__(self, batch_info, img_preprocess_fn, **kwargs):
         data = []
-        num_records = len(batch_info['img_path'])
+        num_records = len(batch_info["img_path"])
         for i in range(num_records):
             record = {
-                'did': batch_info['did'][i],
-                'img_path': batch_info['img_path'][i],
-                'modality': batch_info['modality'][i],
-                'txt': batch_info['txt'][i],
+                "did": batch_info["did"][i],
+                "img_path": batch_info["img_path"][i],
+                "modality": batch_info["modality"][i],
+                "txt": batch_info["txt"][i],
             }
             data.append(record)
         self.data = data
         self.img_preprocess_fn = img_preprocess_fn
         self.kwargs = kwargs
-      
-    def __len__(self):  
-        return len(self.data)  
-      
-    def __getitem__(self, idx):  
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
         entry = self.data[idx]
         img_path = entry.get("img_path", None)
         if not img_path:
@@ -40,7 +40,7 @@ class CustomCorpusDataset(Dataset):
         else:
             img = Image.open(img_path).convert("RGB")
             img = self.img_preprocess_fn(img)
-        
+
         did = entry.get("did", None)
         did = hash_did(did)
         cand_txt = entry.get("txt", "")
@@ -60,8 +60,10 @@ class CustomCorpusDataset(Dataset):
 class UniIRDatasetConverter:
     def __init__(self, batch_info, img_preprocess_fn, tokenizer, **kwargs):
         dataset = CustomCorpusDataset(batch_info, img_preprocess_fn, **kwargs)
-        batch_size = len(batch_info['img_path'])
-        collator = MBEIRCandidatePoolCollator(tokenizer=tokenizer, image_size=(224, 224))
+        batch_size = len(batch_info["img_path"])
+        collator = MBEIRCandidatePoolCollator(
+            tokenizer=tokenizer, image_size=(224, 224)
+        )
         self.data = DataLoader(dataset, batch_size=batch_size, collate_fn=collator)
 
     def get_data(self):
@@ -77,7 +79,7 @@ MODEL_REGISTRY = {
 
 
 class UniIREncoder(ABC):
-    def __init__(self, model_name: str, device='cuda:0', l2_norm=False, **kwargs: Any):
+    def __init__(self, model_name: str, device="cuda:0", l2_norm=False, **kwargs: Any):
         clip_vision_model = "ViT-L/14" if "large" in model_name else "ViT-B/32"
 
         model_key = next((key for key in MODEL_REGISTRY if key in model_name), None)
@@ -90,12 +92,18 @@ class UniIREncoder(ABC):
         try:
             checkpoint_path = hf_hub_download(
                 repo_id="TIGER-Lab/UniIR",
-                filename=f"checkpoint/{model_dir}/{model_name}.pth"
+                filename=f"checkpoint/{model_dir}/{model_name}.pth",
             )
         except Exception as e:
-            raise ValueError(f"Model checkpoint not found: {e}. Please check the model name or ensure the model is available on Hugging Face Hub: https://huggingface.co/TIGER-Lab/UniIR/tree/main/checkpoint.")
+            raise ValueError(
+                f"Model checkpoint not found: {e}. Please check the model name or ensure the model is available on Hugging Face Hub: https://huggingface.co/TIGER-Lab/UniIR/tree/main/checkpoint."
+            )
 
-        model.load_state_dict(torch.load(checkpoint_path, map_location=device, weights_only=False)["model"])
+        model.load_state_dict(
+            torch.load(checkpoint_path, map_location=device, weights_only=False)[
+                "model"
+            ]
+        )
         model.eval()
         model = model.to(device)
 
@@ -109,17 +117,23 @@ class UniIREncoder(ABC):
     def encode(self, **kwargs: Any):
         pass
 
+
 class UniIRCorpusEncoder(UniIREncoder):
-    def __init__(self, model_name: str, device='cuda:0', l2_norm=False, **kwargs: Any):
+    def __init__(self, model_name: str, device="cuda:0", l2_norm=False, **kwargs: Any):
         super().__init__(model_name, device, l2_norm, **kwargs)
 
     def encode(self, dids, img_paths, modalitys, txts, **kwargs: Any):
-        if kwargs.get('fp16', False):
+        if kwargs.get("fp16", False):
             self.model.half()
         else:
             self.model.float()
 
-        batch_info = {"did": dids, "img_path": img_paths, "modality": modalitys, "txt": txts}
+        batch_info = {
+            "did": dids,
+            "img_path": img_paths,
+            "modality": modalitys,
+            "txt": txts,
+        }
         dataset = UniIRDatasetConverter(
             batch_info=batch_info,
             img_preprocess_fn=self.img_preprocess_fn,
@@ -134,5 +148,5 @@ class UniIRCorpusEncoder(UniIREncoder):
             corpus_embeddings, _ = self.model.encode_mbeir_batch(batch)
             corpus_embeddings = corpus_embeddings.cpu().numpy()
             if self.l2_norm:
-                corpus_embeddings = normalize(corpus_embeddings, axis=1, norm='l2')
-            return corpus_embeddings 
+                corpus_embeddings = normalize(corpus_embeddings, axis=1, norm="l2")
+            return corpus_embeddings
